24ad767838afeb2b28b3a50951834b7baefbb724
Florian Thomas <flthomas@students.uni-mainz.de>
2021-07-19 18:24:27 +0200

    Add fully harmonic axial motion

--- cresana/axialmotion.py
+++ cresana/axialmotion.py
@@ -1,4 +1,25 @@
 
-def test():
 
-    print('test')
+"""
+
+Author: F. Thomas
+Date: July 19, 2021
+
+"""
+
+__all__ = []
+
+import numpy as np
+
+def get_omega_axial(v_0, theta_bot, L_0):
+
+    return v_0*np.sin(theta_bot)/L_0
+
+def get_z_max(theta_bot, L_0):
+
+    return L_0/np.tan(theta_bot)
+
+def get_z(t, z_max, omega_a, phi_a):
+
+    return z_max*np.sin(omega_a*t + phi_a)
+
