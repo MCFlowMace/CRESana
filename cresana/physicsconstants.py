eb19302c7afaa3cc2ee4957e18f3695374165d42
Florian Thomas <flthomas@students.uni-mainz.de>
2021-07-19 18:55:33 +0200

    Add physical constants

---
+++ cresana/physicsconstants.py
@@ -0,0 +1,18 @@
+
+
+"""
+
+Author: F. Thomas
+Date: July 19, 2021
+
+"""
+
+__all__ = []
+
+import numpy as np
+
+
+E0_electron = 0.511e6 # electron rest energy 0.511 MeV
+speed_of_light = 299792458 # speed of light m/s
+epsilon0 = 55.263e6 # electric field constant e^2/(eV m)
+ev = 1.602e-19 # eV in J
