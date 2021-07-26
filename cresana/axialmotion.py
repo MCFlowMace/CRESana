ee14b554720eb57cbc759769a2b8e98ddb8c085d
Florian Thomas <flthomas@students.uni-mainz.de>
2021-07-26 14:58:06 +0200

    Add flat axial motion

--- cresana/axialmotion.py
+++ cresana/axialmotion.py
@@ -10,6 +10,7 @@ Date: July 19, 2021
 __all__ = []
 
 import numpy as np
+from scipy.signal import sawtooth
 
 from .physicsconstants import speed_of_light, E0_electron
 
@@ -38,3 +39,13 @@ def get_relativistic_velocity(E_kin):
     relative_energy = E0_electron/(E0_electron + E_kin)
 
     return np.sqrt(1-relative_energy**2)*speed_of_light
+
+def get_z_flat(t, z_max, omega_a, phi_a):
+
+    return z_max*sawtooth(t*omega_a + np.pi/2 + phi_a, width=0.5)
+
+def get_omega_flat(E_kin, theta_bot, L):
+
+    v_0 = get_relativistic_velocity(E_kin)
+
+    return v_0*np.cos(theta_bot)*np.pi/L
