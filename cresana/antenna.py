ce5fa4202bbf58fc4e6099561219d169a830dffa
Florian Thomas <flthomas@students.uni-mainz.de>
2021-08-23 18:13:35 +0200

    Add reference for disk solid angle calculation

--- cresana/antenna.py
+++ cresana/antenna.py
@@ -51,6 +51,7 @@ def get_disc_solid_angle_general(d, R, r):
     r - radial position of the source
     """
 
+    # reference: https://aip.scitation.org/doi/pdf/10.1063/1.1716590
 
     def K(k):
         return special.ellipk(k**2)
