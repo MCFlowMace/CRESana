a605fc56a97e4f6d617b74760f2b9e3b8bd7d995
Florian Thomas <flthomas@students.uni-mainz.de>
2021-08-25 19:49:57 +0200

    Replace formulas for slope and radiated power

--- cresana/cyclotronmotion.py
+++ cresana/cyclotronmotion.py
@@ -28,20 +28,33 @@ def get_avg_omega_cyclotron(B, E_kin, z_max, L_0):
 
     return w0*(1+ z_max**2/(2*L_0**2))
 
-def get_radiated_power(E_kin, theta, B, z_max, L_0):
+# ~ def get_radiated_power(E_kin, theta, B, z_max, L_0):
+
+    # ~ v_0 = get_relativistic_velocity(E_kin)
+    # ~ beta = v_0/speed_of_light
+
+    # ~ scaling_factor = beta**2*np.sin(theta)**2/(1-beta**2)
+
+    # ~ w_0 = get_avg_omega_cyclotron(B, E_kin, z_max, L_0)
+
+    # ~ return w_0**2/(6*np.pi*epsilon0*speed_of_light)*scaling_factor
+
+# ~ def get_slope(E_kin, theta, B, z_max, L_0):
+
+    # ~ p = get_radiated_power(E_kin, theta, B, z_max, L_0)
+    # ~ w_0 = get_avg_omega_cyclotron(B, E_kin, z_max, L_0)
+
+    # ~ return p*w_0/(E0_electron + E_kin)
+    
+def get_radiated_power(E_kin, theta, B, w):
 
     v_0 = get_relativistic_velocity(E_kin)
     beta = v_0/speed_of_light
 
     scaling_factor = beta**2*np.sin(theta)**2/(1-beta**2)
 
-    w_0 = get_avg_omega_cyclotron(B, E_kin, z_max, L_0)
+    return w**2/(6*np.pi*epsilon0*speed_of_light)*scaling_factor
 
-    return w_0**2/(6*np.pi*epsilon0*speed_of_light)*scaling_factor
+def get_slope(E_kin, p, w):
 
-def get_slope(E_kin, theta, B, z_max, L_0):
-
-    p = get_radiated_power(E_kin, theta, B, z_max, L_0)
-    w_0 = get_avg_omega_cyclotron(B, E_kin, z_max, L_0)
-
-    return p*w_0/(E0_electron + E_kin)
+    return p*w/(E0_electron + E_kin)
