f3e253f2765945f4b66e15b634a2a2bdf7293689
Florian Thomas <flthomas@students.uni-mainz.de>
2021-09-28 20:09:25 +0200

    Modify electron rest mass

--- cresana/physicsconstants.py
+++ cresana/physicsconstants.py
@@ -12,7 +12,7 @@ __all__ = []
 import numpy as np
 
 
-E0_electron = 0.511e6 # electron rest energy 0.511 MeV
+E0_electron = 0.51099895000e6 #0.511e6 # electron rest energy 0.511 MeV
 speed_of_light = 299792458 # speed of light m/s
 epsilon0 = 55.263e6 # electric field constant e^2/(eV m)
 ev = 1.602e-19 # eV in J
