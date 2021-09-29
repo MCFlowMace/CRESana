5604cdee509fd789f54e57bed65c613fdd857419
Florian Thomas <flthomas@students.uni-mainz.de>
2021-09-29 14:12:58 +0200

    Fix electric constant
    Use more precise value for epsilon0

--- cresana/physicsconstants.py
+++ cresana/physicsconstants.py
@@ -14,5 +14,5 @@ import numpy as np
 
 E0_electron = 0.51099895000e6 # electron rest energy 0.511 MeV
 speed_of_light = 299792458 # speed of light m/s
-epsilon0 = 55.263e6 # electric field constant e^2/(eV m)
+epsilon0 = 55.26349406e6 # electric field constant e^2/(eV m)
 ev = 1.602e-19 # eV in J
