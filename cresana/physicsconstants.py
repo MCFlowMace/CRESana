a83480e0f2a0b0633e4202625e999bde0efa22b8
Florian Thomas <flthomas@students.uni-mainz.de>
2021-10-25 21:25:43 +0200

    Update constants to pdg2021

--- cresana/physicsconstants.py
+++ cresana/physicsconstants.py
@@ -11,11 +11,15 @@ __all__ = []
 
 import numpy as np
 
-# https://pdg.lbl.gov/2018/reviews/rpp2018-rev-phys-constants.pdf
-
-E0_electron = 0.5109989461e6 # electron rest energy 0.511 MeV
+### https://pdg.lbl.gov/2018/reviews/rpp2018-rev-phys-constants.pdf
+# https://pdg.lbl.gov/2021/tables/rpp2021-sum-leptons.pdf
+#https://pdg.lbl.gov/2021/reviews/rpp2020-rev-phys-constants.pdf
+              
+#E0_electron = 0.5109989461e6 # electron rest energy 0.511 MeV
+E0_electron = 0.51099895e6
 speed_of_light = 299792458 # speed of light m/s
 epsilon0 = 55.26349406e6 # electric field constant e^2/(eV m)
 #ev = 1.602e-19 # eV in J
 
-ev = 1.6021766208e-19 # eV in J
+#ev = 1.6021766208e-19 # eV in J
+ev = 1.602176634e-19
