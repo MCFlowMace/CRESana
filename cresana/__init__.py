05ea868a78163a5835ac42a0701af440573f0efe
Florian Thomas <flthomas@students.uni-mainz.de>
2021-12-22 15:46:28 +0100

    Modify import logic

--- cresana/__init__.py
+++ cresana/__init__.py
@@ -9,4 +9,5 @@ Date: July 19, 2021
 #from .axialmotion import
 
 from .trap import HarmonicTrap, BoxTrap, BathtubTrap, ArbitraryTrap
-from .electronsim import Electron, simulate_electron
+from .electronsim import Electron, simulate_electron, read_kass_sim, ElectronSim
+from .sampling import SignalModel
