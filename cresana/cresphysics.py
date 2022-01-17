3a598381d91f0e0defcded2f157a932058247aa9
Florian Thomas <flthomas@students.uni-mainz.de>
2022-01-17 13:40:03 +0100

    Add directive gain to cresphysics

--- cresana/cresphysics.py
+++ cresana/cresphysics.py
@@ -23,6 +23,11 @@ def get_relativistic_velocity(E_kin):
     return np.sqrt(1-relative_energy**2)*speed_of_light
     
 
+def get_beta(E_kin):
+    v_0 = get_relativistic_velocity(E_kin)
+    return v_0/speed_of_light
+    
+
 def get_omega_cyclotron(B, E_kin):
     '''
     B - magnetic field value in T
@@ -38,12 +43,44 @@ def get_omega_cyclotron_time_dependent(B, E_kin, p, t):
     
 
 def get_radiated_power(E_kin, theta, B):
-    v_0 = get_relativistic_velocity(E_kin)
-    beta = v_0/speed_of_light
+    beta = get_beta(E_kin)
     w0 = get_omega_cyclotron(B, 0.0)
     scaling_factor = beta**2*np.sin(theta)**2/(1-beta**2)
 
     return w0**2/(6*np.pi*epsilon0*speed_of_light)*scaling_factor
+    
+    
+def get_directive_gain(E_kin, pitch, angle):
+    """
+    https://journals.aps.org/pra/abstract/10.1103/PhysRevA.36.1498
+    """
+    def f_factor(beta_parallel, beta_ortho, theta):
+    
+        g_parallel = 1-beta_parallel*np.cos(theta)
+        
+        denominator = 4*(g_parallel**2-beta_ortho**2*np.sin(theta)**2)**(7/2)
+        
+        enum1 = 4*g_parallel**2*((1+beta_parallel**2)*(1+np.cos(theta)**2) - 4*beta_parallel*np.cos(theta))
+        enum2 = (1- beta_parallel**2 + 3*beta_ortho**2)*beta_ortho**2*np.sin(theta)**4
+        
+        enum = enum1 - enum2
+        
+        return enum/denominator
+    
+    def g_factor(beta_parallel, beta_ortho, theta):
+        
+        beta = np.sqrt(beta_parallel**2 + beta_ortho**2)
+        
+        return 0.75*(1-beta**2)**2*f_factor(beta_parallel, beta_ortho, theta)
+        
+    def doppler_correction(g, beta_parallel, theta):
+        return 1/(1 - beta_parallel*np.cos(theta))*g
+        
+    beta = get_beta(E_kin)
+    beta_parallel = beta*np.cos(pitch)
+    beta_ortho = beta*np.sin(pitch)
+    g = g_factor(beta_parallel, beta_ortho, angle)
+    return doppler_correction(g, beta_parallel, angle)
     
 
 def get_slope(E_kin, p, w):
