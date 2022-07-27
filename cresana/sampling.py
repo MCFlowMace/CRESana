59dd3e90bd4d8818a413b4355db04407f4c56111
Florian Thomas <flthomas@students.uni-mainz.de>
2022-07-27 11:10:16 -0400

    Add new simulation class

--- cresana/sampling.py
+++ cresana/sampling.py
@@ -16,6 +16,7 @@ import matplotlib.pyplot as plt
 from .physicsconstants import speed_of_light, ev
 from .cyclotronphysics import get_radiated_power, get_omega_cyclotron_time_dependent
 from .electronsim import simulate_electron
+from .cyclotronphysics import AnalyticCyclotronField
 
 
 class Clock:
@@ -45,7 +46,7 @@ class Clock:
         self._dt = val
         self._sr = 1/val
         
-    @sr.setter
+    @f.setter
     def f(self, val):
         self._f = val
         self._dt = 1/val
@@ -106,7 +107,94 @@ def find_nearest_samples2d(t1, t2):
         t[i], ind[i] = find_nearest_samples(t1[i], t2)
         
     return t, ind
+
+
+class Simulation:
+
+    def __init__(self, antenna_array, sampling_rate, f_LO):
+        self.clock = Clock(sampling_rate)
+        self.antenna_array = antenna_array
+        self.receiver = IQReceiver(f_LO)
         
+    def get_sample_time_trajectory(self, N, electron_sim):
+        t, sample_ind = find_nearest_samples(self.clock(N), electron_sim.t)
+        coords = electron_sim.coords[sample_ind]
+        
+        return t, coords
+    
+    def get_retarded_time(self, t, d):
+        t_travel = d/speed_of_light
+        t_ret = t - t_travel
+        
+        return t_ret
+    
+    #~ def calc_antenna_dist(self, coords):
+        #~ self.dist_cache = self.antenna_array.get_distance(coords)
+        #~ self.d_abs_cache = np.sqrt(np.sum(self.dist_cache**2, axis=-1))
+        
+    def enforce_causality(self, t_retarded, t_ret_correct, B_sample, pitch):
+        
+        # setting to zero adds a small error for the initial phase in the phase integral
+        # since this way the integral starts from t_ret=0 (which is correct) but 
+        # it is assumed that omega(t_ret=0) = 0
+        # the alternative solution of passing only the causal indices to the integral
+        # has a wrong initial phase as well since it starts the integral from
+        # some t_ret>0. The correct solution would be to find the correct value of omega(t_ret=0)
+        # and integrating from there
+        ind_non_causal = t_retarded<0
+        B_sample[ind_non_causal] = 0
+        t_ret_correct[ind_non_causal] = 0
+        
+    def get_sampled_model_parameters(self, electron_sim, t_retarded):
+
+        t_ret_correct, sample_ind_correct = find_nearest_samples2d(t_retarded, electron_sim.t)
+        B_sample = electron_sim.B_vals[sample_ind_correct]
+        pitch = electron_sim.pitch[sample_ind_correct]
+        E_kin = elctron_sim.E_kin[sample_ind_correct]
+        
+        self.enforce_causality(t_retarded, t_ret_correct, B_sample, pitch)
+        
+        return t_ret_correct, B_sample, pitch, E_kin
+        
+    def calc_d_vec_and_abs(self, coords):
+        r = np.expand_dims(self.antenna_array.positions, 1) - coords
+        
+        d = np.sqrt(norm_squared(r))
+        d_vec = r/d
+        
+        return d_vec, d
+        
+    def calc_polar_angle(self, r_norm, B_direction):
+
+        return np.arccos(np.dot(r_norm, B_direction))
+
+    def get_samples(self, N, electron_sim):
+        
+        t, coords = self.get_sample_time_trajectory(N, electron_sim)
+        
+        d_vec, d = self.calc_d_vec_and_abs(coords)
+        theta = self.calc_polar_angle(d_vec, electronsim.B_direction)
+        
+        t_ret_initial = self.get_retarded_time(t, coords)
+
+        t_ret, B_ret, pitch_ret, E_kin_ret = self.get_sampled_model_parameters(electron_sim, t_ret_initial)
+
+                                                
+        retarded_electron_sim = ElectronSim(coords, t_ret, B_ret, E_kin_ret, 
+                                            pitch_ret, electron_sim.B_direction)
+        
+        cyclotron_field = AnalyticCyclotronField(retarded_electron_sim)
+        
+        w, P_transmitted, pol_x, pol_y = cyclotron_field.get_field_parameters(d_vec, d, theta)
+                                                
+        received_copolar_field_power = self.antenna_array.get_received_copolar_field_power(P_transmitted, w, pol_x, pol_y, d)
+        field_phase = get_cyclotron_phase_int(w, t_ret_correct)
+        
+        signal = self.receiver(t, self.antenna_array, received_copolar_field_power, 
+                                field_phase, d_vec)
+
+        return signal
+
 
 class SignalModel:
 
