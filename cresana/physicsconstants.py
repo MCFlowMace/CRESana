91130db4c10bc4deb10ac505ebae16c3b7f9ea4e
Florian Thomas <flthomas@students.uni-mainz.de>
2022-08-18 15:28:50 -0400

    Some cleanup in physicsconstants

--- cresana/physicsconstants.py
+++ cresana/physicsconstants.py
@@ -15,12 +15,17 @@ import numpy as np
 ### https://pdg.lbl.gov/2018/reviews/rpp2018-rev-phys-constants.pdf
 # https://pdg.lbl.gov/2021/tables/rpp2021-sum-leptons.pdf
 #https://pdg.lbl.gov/2021/reviews/rpp2020-rev-phys-constants.pdf
-              
-#E0_electron = 0.5109989461e6 # electron rest energy 0.511 MeV
+
+#pdg 2021
 E0_electron = 0.51099895e6
 speed_of_light = 299792458 # speed of light m/s
 epsilon0 = 55.26349406e6 # electric field constant e^2/(eV m)
-#ev = 1.602e-19 # eV in J
-
-#ev = 1.6021766208e-19 # eV in J
 ev = 1.602176634e-19
+
+#~ #kass-locust
+#~ E0_electron = 0.510998918e6
+#~ speed_of_light = 299792458 # speed of light
+#~ epsilon0 = 55.26349406e6 # electric field constant e^2/(eV m) (this is pdg2021 not kassiopeia)
+#~ ev = 1.60217653e-19
+
+#~ #epsilon0 = 8.854187817e-12 #this is kassiopeia but conversion to e^2/(eV m) necessary
