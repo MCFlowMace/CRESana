5f786b44754c18723a3598548e86f653cefe3847
Florian Thomas <fthomas@uni-mainz.de>
2023-03-28 16:15:48 +0200

    Add import of GenericAntenna

--- cresana/__init__.py
+++ cresana/__init__.py
@@ -9,5 +9,5 @@ Date: July 19, 2021
 
 from .electronsim import Electron, AnalyticSimulation, KassSimulation
 from .sampling import Simulation
-from .antenna import AntennaArray, SlottedWaveguideAntenna, IsotropicAntenna
+from .antenna import AntennaArray, SlottedWaveguideAntenna, IsotropicAntenna, GenericAntenna
 from .trap import HarmonicTrap, BoxTrap, BathtubTrap
